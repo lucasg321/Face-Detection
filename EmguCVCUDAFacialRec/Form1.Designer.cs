0d0f1f64bb919e4ae5ba9bcd8ff725a250720b64
Lucas Gigliozzi <lucasg@esna.com>
2019-11-02 18:12:43 -0400

    Detecting face and sending coordinates -- in progress

---
+++ EmguCVCUDAFacialRec/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace EmguCVCUDAFacialRec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(600, 400);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(652, 12);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(600, 400);
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 443);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 60);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start Feed";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 527);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 59);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Start Facial Detection Haar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(12, 610);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 59);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Start Facial Detection LBP";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(160, 443);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 60);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Pause";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AccessibleName = "textBox1";
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(160, 527);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 59);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.Location = new System.Drawing.Point(160, 610);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 59);
+            this.textBox2.TabIndex = 9;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(1110, 443);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(142, 60);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Aim";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(1110, 609);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(142, 60);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Fire";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // serialPort1
+            // 
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ EmguCVCUDAFacialRec/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.Cuda;
+using System.Timers;
+using System.Windows;
+using System.Runtime.InteropServices;
+
+
+
+namespace EmguCVCUDAFacialRec
+{
+    public partial class Form1 : Form
+    {
+        Mat m1 = new Mat();
+        System.Timers.Timer aTimer = new System.Timers.Timer();
+        System.Timers.Timer hTimer = new System.Timers.Timer();
+
+        VideoCapture capture;
+        bool pause = false;
+        bool haarDetect = false;
+        bool lbpDetect = false;
+
+        int X = 0, Y = 0;
+
+        Image<Bgr, Byte> m_cam;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try { 
+            pause = false;
+            capture = new VideoCapture();
+            Mat m = new Mat();
+            capture.Read(m);
+            imageBox1.Image = m;
+            aTimer.Elapsed += new ElapsedEventHandler(imageCapture);
+            aTimer.Interval = 500;
+            aTimer.Enabled = true;
+        }
+                     catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+
+            }
+        }
+
+        private void imageCapture(object source, ElapsedEventArgs e)
+        { 
+           
+            try
+            { 
+                while (!pause)
+                {
+                    capture.Read(m1);
+                    
+                    if (!m1.IsEmpty)
+                    {
+                        imageBox1.Image = m1;
+                        double fps = capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps);
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    if (haarDetect == true)
+                    {
+                        DetectFaceHaar();
+                    }
+                    if (lbpDetect == true)
+                    {
+                        DetectFaceLBP();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+               // MessageBox.Show(ex.Message);
+
+            }
+            return;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            haarDetect = true;
+        }
+        public void DetectFaceHaar()
+        { 
+           
+            try 
+            {
+                string facePath = Path.GetFullPath(@"../../data/haarcascade_frontalface_default.xml");
+                string eyePath = Path.GetFullPath(@"../../data/haarcascade_eye.xml");
+
+                CascadeClassifier classifierFace = new CascadeClassifier(facePath);
+                CascadeClassifier classifierEye = new CascadeClassifier(eyePath);
+
+                var imgGray = m1.ToImage<Bgr, Byte>().Convert<Gray, byte>().Clone();
+                Rectangle[] faces =  classifierFace.DetectMultiScale(imgGray, 1.1,5);
+                m_cam = m1.ToImage<Bgr, Byte>();
+
+                foreach (var face in faces)
+                {
+                    m_cam.Draw(face, new Bgr(0, 0, 255), 2);
+
+                    imgGray.ROI = face;
+
+                Rectangle[] eyes = classifierEye.DetectMultiScale(imgGray, 1.1, 5); 
+                    
+                    foreach(var eye in eyes)
+                    {
+                        var ee = eye;
+                        ee.X += face.X;
+                        ee.Y += face.Y;
+                        m_cam.Draw(ee, new Bgr(0, 255, 0), 2);
+
+                        X = ee.X;
+                        Y = ee.Y;
+                    }
+                } 
+                imageBox2.Image = m_cam;
+               
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+                lbpDetect = true;
+      
+        }
+        public void DetectFaceLBP()
+        {
+            try
+            {
+                string facePath = Path.GetFullPath(@"../../data/lbpcascade_frontalface.xml");
+
+                CascadeClassifier classifierFace = new CascadeClassifier(facePath);
+
+                var imgGray = m1.ToImage<Bgr, Byte>().Convert<Gray, byte>().Clone();
+                Rectangle[] faces = classifierFace.DetectMultiScale(imgGray, 1.1, 4);
+                m_cam = m1.ToImage<Bgr, Byte>();
+                foreach (var face in faces)
+                {
+                    m_cam.Draw(face, new Bgr(255, 0, 0), 2);
+
+                }
+                imageBox2.Image = m_cam;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pause = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "X Value: " + X + "                      "; //enough to make new line: "                      "
+            textBox1.Text += "Y Value: " + Y;
+
+            String xstring = "X" + X.ToString();
+            String ystring = "Y" + Y.ToString();
+            String xystring = xstring + ystring;
+
+            //serialPort1.Write(xystring);//send x and y values to serial port
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            String fire = "fire";
+          //  serialPort1.Write(fire);//send trigger word "fire" to serial port
+        }
+
+    }
+}
